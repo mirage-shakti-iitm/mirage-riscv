20dd63d6a402cf184f1613f15f5cea27eff65cd1
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2019-08-16 14:39:19 +0530

    print sleep_queue

--- lib/time.ml
+++ lib/time.ml
@@ -114,7 +114,7 @@ let rec print_list = function
 | e::l -> print_int e ; print_string " " ; print_list l
 
 let rec get_next_timeout () =
-  let ll = String.concat "," (sleep_queue);
+(*   let ll = String.concat "," (sleep_queue); *)
    
   match SleepQueue.maximum sleep_queue with
   | exception Binary_heap.Empty -> None
