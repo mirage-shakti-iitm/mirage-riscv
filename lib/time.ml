844b1e8a61bbd5fc25eec835494a26122e146689
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2019-08-19 12:58:55 +0530

    inspecting restart_threads

--- lib/time.ml
+++ lib/time.ml
@@ -73,11 +73,11 @@ let new_sleeps = ref []
 
 let sleep_ns d =
   Logs.info (fun f -> f "sleep_ns : d: %s" (Int64.to_string d)) ;
-  let converted_d = Int64.of_int (Int64.to_int (d) / 90) in
+  (* let converted_d = Int64.of_int (Int64.to_int (d) / 90) in *)
   let (res, w) = Lwt.task () in
-  Logs.info (fun f -> f "sleep_ns : converted_d: %s" (Int64.to_string converted_d)) ;
-  Logs.info (fun f -> f "sleep_ns : org_time: %s" (Int64.to_string Monotonic.(time() + of_nanoseconds 0L)));
-  let t = Monotonic.(time () + of_nanoseconds converted_d) in
+  (* Logs.info (fun f -> f "sleep_ns : converted_d: %s" (Int64.to_string converted_d)) ; *)
+  Logs.info (fun f -> f "sleep_ns : mtime: %s" (Int64.to_string Monotonic.(time() + of_nanoseconds 0L)));
+  let t = Monotonic.(time () + of_nanoseconds d) in
   (* let t = Monotonic.(time () + of_nanoseconds 0L) in *)
   Logs.info (fun f -> f "sleep_ns : t: %s" (Int64.to_string t));
   let sleeper = { time = converted_d; canceled = false; thread = w } in
@@ -129,7 +129,7 @@ let select_next () =
   (* Transfer all sleepers added since the last iteration to the main
      sleep queue: *)
   let t = Monotonic.(time () + of_nanoseconds 0L) in
-  Logs.info (fun f -> f "sleep_ns : t: %s" (Int64.to_string t));   
+  Logs.info (fun f -> f "select_next : mtime: %s" (Int64.to_string t));   
   List.iter
       (fun e -> SleepQueue.add sleep_queue e) !new_sleeps;
   new_sleeps := [];
